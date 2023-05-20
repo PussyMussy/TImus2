228d2e9ddb212922c5b08ccefeeeae6e389e22d6
PussyMussy <MaxWar228@mail.ru>
2023-05-21 00:02:40 +0500

    Timus2

--- src/main/java/org/example/Povorot_na_MEGU_1787.java
+++ src/main/java/org/example/Povorot_na_MEGU_1787.java
@@ -1,2 +1,39 @@
-package org.example;public class Povorot_na_MEGU_1787 {
+package org.example;
+
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Povorot_na_MEGU_1787 {
+
+    static Scanner input = new Scanner(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String str[]) {
+        Locale.setDefault(Locale.US);
+
+        final int k = input.nextInt();
+        final int n = input.nextInt();
+
+        int cars[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            cars[i] = input.nextInt();
+        }
+
+        int cur = 0;
+        for (int i = 0; i < n; i++) {
+            cur += cars[i];
+            if ((cur - k) < 0) {
+                cur = 0;
+            } else {
+                cur -= k;
+            }
+        }
+        out.println(cur);
+        out.flush();
+    }
 }
+
+
+
+
--- src/main/java/org/example/Uralskie_bifteksi_1820.java
+++ src/main/java/org/example/Uralskie_bifteksi_1820.java
@@ -1,2 +1,40 @@
-package org.example;public class Uralskie_bifteksi_1820 {
+package org.example;
+
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Uralskie_bifteksi_1820 {
+
+    static Scanner input = new Scanner(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static final int calcTime(final int time, int n, int k) {
+        if (n <= 0) {
+            return 0;
+        } else if (n < k) {
+            return 2 * time;
+        } else {
+            int appender = 0;
+            int n2 = n << 1;
+            if (((n2 % k)) > 0) {
+                appender = 1;
+            }
+            return n2 / k + appender;
+        }
+    }
+
+    public static void main(String str[]) {
+        Locale.setDefault(Locale.US);
+
+        int n = input.nextInt();
+        int k = input.nextInt();
+
+        final int time = 1;
+
+
+        out.println(calcTime(time, n, k));
+        out.flush();
+    }
 }
+
